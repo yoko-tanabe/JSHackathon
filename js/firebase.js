49cd1c8cbcf932815f51d1ca1efa263efd4f76b1
yoko-tanabe <yoko.tanabe.213@gmail.com>
2026-01-05 23:43:33 +0900

    提出前の最終のコード

---
+++ js/firebase.js
@@ -0,0 +1,238 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+import { getDatabase, ref, push, set, onChildAdded, onChildChanged, remove, onChildRemoved, update }
+    from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBFnmrWfPrFyW-NEj7SITL1T_00hww-B0g",
+    authDomain: "dev30-f18-23c94.firebaseapp.com",
+    projectId: "dev30-f18-23c94",
+    storageBucket: "dev30-f18-23c94.firebasestorage.app",
+    messagingSenderId: "867812372908",
+    appId: "1:867812372908:web:98218493ec3170451b0a09"
+};
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app); //RealtimeDBに接続
+const dbRef = ref(db, "data"); //RealtimeDB内の"data"を使う
+
+
+//データ登録(Click)
+//送信
+
+let individualData = Array(3);
+
+$('.message_send_btn').on('click', function () {
+
+
+
+    //疑問 Local Storageのデータは送れないのかなあ
+    let ppl = Number($('#num_of_people').val());
+    let textdata = '';
+
+    for (i = 0; i < 3; i++) {
+        individualData[i] = {
+            checked : false,
+            name: $(`#people_name_${i}`).val(),
+            pay: Number($(`#people_exp_${i}`).val())
+        }
+        //最初以下のようにやったが、中身が以下のようになって不適切
+        //[object Object],[object Object]
+        // textdata += `${individualData[i]},`
+    }
+
+    console.log("データベース準備中");
+    console.log(individualData);
+    // console.log(textdata);
+
+
+    let msg = {
+        date: $('#date_and_year').val(),
+        account: $('#account_info').val(),
+        message: $('#thank_you_message').val(),
+        date: $('#date_and_year').val(),
+        ppl: $('#num_of_people').val(),
+        //Firebaseはjsonをそのまま保存できる
+        person: individualData
+
+    }
+
+    const newPostRef = push(dbRef)
+    set(newPostRef, msg)
+
+});
+
+//この下は消さない
+
+
+//データ表示(Enter)
+onChildAdded(dbRef, function (data) {
+
+    const msg = data.val()
+    const key = data.key //keyの部分は課題またはごじしんで使用いただくための
+
+
+
+    //削除していいはず
+    // const dbRef_d = ref(db, "data"); //RealtimeDB内の"data"を使う
+
+    //     //疑問 Local Storageのデータは送れないのかなあ
+    //     let ppl = Number($('#num_of_people').val());
+    //     let individualData = Array(3);
+    //     let textdata = '';
+
+    //     for (i = 0; i < 3; i++) {
+    //         individualData[i] = {
+    //             name: $(`#people_name_${i}`).val(),
+    //             pay: Number($(`#people_exp_${i}`).val())
+    //         }
+    //         //最初以下のようにやったが、中身が以下のようになって不適切
+    //         //[object Object],[object Object]
+    //         // textdata += `${individualData[i]},`
+    //     }
+
+
+    // console.log("データベース準備中");
+    // console.log(individualData);
+    // // console.log(textdata);
+
+
+    // let msg_d = {
+    //     date: $('#date_and_year').val(),
+    //     ppl: $('#num_of_people').val(),
+    //     //Firebaseはjsonをそのまま保存できる
+    //     person: individualData
+
+    // }
+
+    // const newPostRef_d = push(dbRef_d)
+    // set(newPostRef_d, msg_d)
+
+
+    //onChildAddedとは
+    //「すでにある子 + 新しく追加された子」を1件ずつ受け取るリスナーなので
+    //基本表示専用である
+
+//条件式？ 式1：式2--> 条件式がTrueなら式1を実行、Falseなら式2を実行
+    let peopleHtml = "";
+
+
+    msg.person.forEach(function (p, i) {
+        peopleHtml += `<p class="stored_data" id="stored_data_${i}">
+    名前 : ${p.name}, 金額 : ${p.pay}円
+    </p>`
+
+    })
+
+    const html = `
+                <div>
+                <p> -------------------------- </p>
+                <h2> ID : ${key} </h2>  
+                <h2> DATE :${msg.date} </h2>  
+                <h2> MESSAGE </h2>  
+                <p> ${msg.message} </p>
+                <p> ${peopleHtml} </p>
+                <p> ${msg.account} </p>
+                <p> -------------------------- </p>
+                <p> -------------------------- </p>
+                </div>
+            `
+
+
+
+
+let html_table_data = "";
+msg.person.forEach(function (p, i) {
+
+    //なんでこっちは動かないのか後でかくにんする
+    // const checkedAttr = ''
+    // if (p.checked === true){checkedAttr ='checked';}
+const checkedAttr= p.checked ? 'checked' : '';
+ 
+
+html_table_data += `
+ <tr class="table_contents">
+            <th class="datacell">
+                <input id="checkbox_${key}_${i}" class="checkbox" type="checkbox" ${checkedAttr}>
+            </th>
+            <th class="datacell">${p.name}</th>
+            <th class="datacell">${p.pay}</th>
+</tr>
+`
+})
+
+const html_table = `
+<table class="table">
+    <thead class="table_thread">
+        <tr class="table_header">
+            <th class="dataheader">完了したらチェック</th>
+            <th class="dataheader">名前</th>
+            <th class="dataheader">金額</th>
+        </tr>
+    </thead>
+    <tbody class="table_tboday">
+ ${html_table_data}
+
+    </tbody>
+
+
+</table>
+`
+
+
+$('.store').append(html_table)
+$('.store').append(html)
+    })
+;
+
+
+//以下のファイルは別フォルダだと動かず、もう一回importとかおまじないをやらないといけなかった
+//ES Modules（import を使っているJS）では、
+//変数は「そのファイルの中だけ」に閉じ込められる
+//動的に追加されたアイテムのonclickには注意！！！！！
+$(document).on('change', '.checkbox',function(){
+////////////////この辺りは若干自信がないので後でかくにんする///////////////////////
+
+//thisでDOM要素をとってきて、その中のidをとってくる
+const id = $(this).attr('id');
+
+//_は慣習的に使わない変数を示したい時
+//_でidを分割する
+const [_, firebaseKey, index] = id.split('_')
+
+//.prop(attr)ではattrの属性値が取得できる
+const checked = $(this).prop('checked');
+
+console.log(firebaseKey, index,checked);
+
+const dbRef_update= ref(db, `data/${firebaseKey}/person/${index}`)
+
+update (dbRef_update,{checked : checked})
+
+});
+
+onChildChanged(dbRef, function(data){
+const msg = data.val();
+const key = data.key();
+
+msg.person.forEach(function(p,i){
+
+    //どのIDのDOM操作をするのかを確認する
+const targetID = `checkbox_${key}_${i}`;
+
+//checkedの状態をDOMに反映する
+$(targetID).prop('checked', !!p,checked);
+
+})
+
+})
+
+//Firebaseに変化が起きた時に、フロントエンド側を変えるにはonAddedChildではできない
+//onChildAddedは初期表示（既存データの表示）、onChildChangedはchecked変更の反映
+//Firebase側のイベントリス名を使う必要がある
+//Firebaseは状態管理ライブラリではないので、自動では反映されないし、勝手にDOMも更新されない
+
+
+//この下は消さない
+;
+
+//この下は消さない
--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@
 //変数の定義　https://magazine.techacademy.jp/magazine/14872
 
 
-let real_name = Array(2);
-let input = Array(2);
-let input_num = Array(2);
-let ratio = Array(2);
-let person_payment = Array(2);
-let total_value = 0;
-let num_ppl = 0;
+let real_name = Array();
+let input = Array();
+let input_num = Array();
+let ratio = Array();
+let person_payment = Array();
+let exp=Array();
+let total_value;
+let num_ppl;
 
 
 //inputでとった数値は文字列なので、数値に変換する
@@ -25,21 +26,56 @@ let num_ppl = 0;
 
 // }
 
-//アカウント情報をvalで撮ってきて、それをLocal Storageに保存する
+
+
+
+//Local Storageから各種情報を読み込んできて、削除ボタンを押さない限りは削除をしないようにする関数
+/**関数 : Local Storage上のKey名と該当する入力欄のclassかIDを入力すると
+ *Local Storageに保存されているデータを持ってきて、入力欄に常に表示するようにする関数
+ * @param {key_for_ls,name} LocalStorage上のKey名と該当する入力欄のclassかID
+ * @returns {} なし 
+ */
+function KeepValue(key_for_ls, name) {
+    if (localStorage.getItem(key_for_ls)) {
+        let saved_info = localStorage.getItem(key_for_ls);
+        $(name).val(saved_info);
+    }
+}
+
+/**関数 : LocalStorageの数値をクリアする関数
+ *Local Storage上のKeyを入力すると、LocalStorageがクリアされる関数
+ *クリアしたLocalStorageに対応する入力欄のIDかclass名を指定するとそこの数値もからになる
+ * @param {key_for_ls, name} LocalStorage上のKey名と該当する入力欄のclassかID
+ * @returns {} なし * 
+ 
+ */
+function removeLocalStorage(key_for_ls, name) {
+    localStorage.removeItem(key_for_ls)
+    $(name).val("")
+}
+
+
+//入力したアカウント情報をvalで撮ってきて、それをLocal Storageに保存する
 $('.save_account_btn').on('click', function () {
     let account_info_val = $('#account_info').val();
     localStorage.setItem("account_information", account_info_val);
 });
 
-if (localStorage.getItem('account_information')) {
-    const saved_account_info = localStorage.getItem("account_information");
-    $('#account_info').val(saved_account_info);
-}
+//+++入力した情報をLocal Storageから持ってきて削除しない限りは表示するようにする
+//関数化けるしたので削除してOK
+// if (localStorage.getItem('account_information')) {
+//     const saved_account_info = localStorage.getItem("account_information");
+//     $('#account_info').val(saved_account_info);
+// }
+
+KeepValue('account_information', '#account_info')
+
 
 $('.rm_account_btn').on('click', function () {
-    localStorage.removeItem('account_information')
-    $('#account_info').val("")
+    removeLocalStorage('account_information','#account_info')
 });
+
+
 
 //テンプレメッセージ情報をvalで撮ってきて、それをLocal Storageに保存する
 $('.save_btn').on('click', function () {
@@ -47,36 +83,69 @@ $('.save_btn').on('click', function () {
     localStorage.setItem("message_template", thx_message_val);
 });
 
-if (localStorage.getItem('message_template')) {
-    const saved_thx_info = localStorage.getItem("message_template");
-    $('#thank_you_message').val(saved_thx_info);
-}
 
-$('.rm_template_message').on('click', function () {
-    localStorage.removeItem('message_template')
-    $('#thank_you_message').val("")
-});
+KeepValue('message_template', '#thank_you_message')
+//+++情報をLocal Storageから持ってきて削除しない限りは表示するようにする
+//関数化けるしたので削除してOK
+// if (localStorage.getItem('message_template')) {
+//     const saved_thx_info = localStorage.getItem("message_template");
+//     $('#thank_you_message').val(saved_thx_info);
+// }
 
-//参加人数をとってくる
+// $('.rm_template_message').on('click', function () {
+//     localStorage.removeItem('message_template')
+//     $('#thank_you_message').val("")
+// });
+
+
+/**$('.rm_template_message').on(
+  'click', 
+  removeLocalStorage('message_template','#thank_you_message')
+);
+のようにすると、Click時ではなくページ読み込み時に記載した関数が発動されてしまう
+
+なんで？
+*/
+
+$('.rm_template_message').on('click', function(){
+removeLocalStorage('message_template','#thank_you_message')
+} ); 
+
+
+//参加人数をとってきてLocal Storageに格納する
 function Getppl() {
     const value_ppl = $('#num_of_people').val();
     const num_ppl = Number(value_ppl);
     return num_ppl;
 }
 
-//参加人数をとってくる
+function SavePpl() {
+    localStorage.setItem("ppl", $('#num_of_people').val());
+}
+
+//参加人数をとってきて、人数分のFormを作る。
 $('#btn_ppl').on('click', function () {
     let num_ppl = Getppl()
     CreateForms(num_ppl)
     UpdateArray(num_ppl)
+    SavePpl()
 });
 
+//+++入力した情報をLocal Storageから持ってきて削除しない限りは表示するようにする
+KeepValue('ppl', '#num_of_people')
+//Numberをつけなくても動いちゃったが、エラーの元なのでNumberをつける
+let keeped_num_ppl = Number(localStorage.getItem('ppl'));
+CreateForms(keeped_num_ppl)
+UpdateArray(keeped_num_ppl)
+console.log("input_numの値ココア" + input_num)
 
-//削除 $('.expected_amount3').append(html);
 
 
 
 function CreateForms(data) {
+
+    //何回も押すとどんどん追加されていってしますので追記する
+    // $('.exp').empty();
 
     for (let i = 0; i < data; i++) {
 
@@ -113,15 +182,18 @@ function ShowResult(data) {
     }
 }
 
-function UpdateArray (data){
-let real_name = Array(data);
-let input = Array(data);
-let input_num = Array(data);
-let ratio = Array(data);
-let person_payment = Array(data);
+function UpdateArray(data) {
+    real_name = Array(data);
+    input = Array(data);
+    input_num = Array(data);
+    ratio = Array(data);
+    person_payment = Array(data);
+    exp=Array(data);
+    console.log("input_numの更新した値は")
+    console.log(input_num)
 }
 
-console.log("num_pplの値は：", num_ppl)
+
 // //参加者の名前を取ってくる
 
 ///なんでこれ動かないのかな
@@ -132,16 +204,19 @@ console.log("num_pplの値は：", num_ppl)
 // });
 // }
 
-$(document).on('change', '.people_name', function () {
+//参加者の名前をとってくる
+//関数化した
+function peopleName() {
 
- let num_ppl = Getppl()
+    let num_ppl = Getppl()
 
     for (let i = 0; i < num_ppl; i++) {
         real_name[i] = $(`#people_name_${i}`).val();
     }
 
     console.log(real_name)
-});
+    return real_name;
+};
 
 
 //////以下削除ーーーー
@@ -157,19 +232,21 @@ $(document).on('change', '.people_name', function () {
 
 
 //みんながどれくらいの負担をするかを記載
-$(document).on('change', '.people_exp', function () {
+//関数化した
+function guessExpense() {
 
-        let num_ppl = Getppl()
+    let num_ppl = Getppl()
 
     for (let i = 0; i < num_ppl; i++) {
         input[i] = $(`#people_exp_${i}`).val();
-        console.log(`change event後のinput${i}の値：` + input[i]);
+        // console.log(`change event後のinput${i}の値：` + input[i]);
         input_num[i] = Number(input[i]);
-        console.log(`change event後のinput${i}の数値：` + input_num[i]);
+        // console.log(`change event後のinput${i}の数値：` + input_num[i]);
     }
 
     console.log(input_num);
-});
+    return input_num;
+};
 
 //////以下削除ーーーー
 // $(`#people1_exp`).on('change', function () {
@@ -194,12 +271,20 @@ $(document).on('change', '.people_exp', function () {
 // });
 //////ここまで削除ーーーー
 
-$('#value').on('change', function () {
+//ここはOnclickに変更した
+//何故なら、総額が変わらなくても負担割合を変えたい場合があるから
+$('#start_btn').on('click', function () {
+real_name=peopleName()
+input_num=guessExpense()
+
+let date_and_year=$('#date_and_year').val();
+localStorage.setItem('DATE',date_and_year)
+
     // console.log('valueを変えた時のinput_1:'+input_1);
     total_value = $('#value').val();
     // console.log('change event後のtotal_valueの値：'+total_value);
     //数値に変換
-    total_value_num = Number(total_value);
+    let total_value_num = Number(total_value);
 
     localStorage.setItem('Total_Pay', total_value_num)
     console.log('total_value_Numの数値：' + total_value_num);
@@ -219,37 +304,37 @@ $('#value').on('change', function () {
     for (i = 0; i < input_num.length; i++) {
         ratio[i] = input_num[i] / sum
         console.log('ratio：' + i + "の値：" + ratio[i]);
+        exp[i] = ratio[i]*sum;
     }
 
+   
 
-});
-
-
-
-$('.start_btn').on('click', function () {
-let num_ppl = Getppl()
+    let num_ppl = Getppl()
+    let total_value_num_ls = Number(localStorage.getItem('Total_Pay'));
     ShowResult(num_ppl)
+
     //計算を開始する
 
-alert("ugoitemasu")
-console.log("person_paymentの値　ここ：" + ratio)
-console.log("total_valueの値 ここ：" + total_value)
+    alert("ugoitemasu")
+    console.log("person_paymentの値　ここ：" + ratio)
+    console.log("total_valueの値 ここ：" + total_value_num_ls)
     for (i = 0; i < input_num.length; i++) {
-        person_payment[i] = ratio[i] * total_value_num
+        person_payment[i] = ratio[i] * total_value_num_ls
         console.log('Person' + i + "の支払額：" + person_payment[i]);
         $(`#people_cal_val_${i}`).html(person_payment[i]);
 
         let obj = {
             name: real_name[i],
-            jpy: person_payment[i]
+            jpy: person_payment[i],
+            exp: exp[i],
         }
         console.log(obj);
 
 
-        let json_fmt = JSON.stringify(obj);
+        let json_str_fmt = JSON.stringify(obj);
         let x = i + 1
         let key = 'person' + x;
-        localStorage.setItem(key, json_fmt);
+        localStorage.setItem(key, json_str_fmt);
     }
 
 
@@ -263,6 +348,53 @@ console.log("total_valueの値 ここ：" + total_value)
     // console.log(obj);
 
 })
+
+//合計金額を削除しない限りは保存する
+KeepValue('Total_Pay', '#value')
+
+//参加者の名前を削除しない限りは保持する
+
+if (localStorage.getItem('Total_Pay')) {
+    let ppl = Number(localStorage.getItem('ppl'));
+    let jsonObj = Array(ppl)
+    let js_pars_obj = Array(ppl)
+
+    for (i = 0; i < ppl; i++) {
+        //str json形式になったものを一行つづ指定して取り出したい場合も
+        jsonObj[i] = localStorage.getItem(`person${i + 1}`);
+        if (jsonObj[i]) {
+            js_pars_obj[i] = JSON.parse(jsonObj[i]);
+        }
+        console.log("jsonObjの値XXX")
+        console.log(jsonObj)
+        $(`#people_name_${i}`).val(js_pars_obj[i].name);
+        $(`#people_exp_${i}`).val(js_pars_obj[i].exp);
+
+    }
+} else { console.log("まだTotal_Playの値がないです"); }
+
+//日時を削除しない限りは保持する
+KeepValue('DATE','#date_and_year')
+
+//案件データを削除する
+$('.rm_contents_btn').on('click', function () {
+let ppl = Number(localStorage.getItem('ppl'));
+localStorage.removeItem('ppl');
+$('#num_of_people').val("");
+localStorage.removeItem('Total_Pay');
+$('#value').val("");
+localStorage.removeItem('DATE');
+$('#date_and_year').val("");
+
+ for(let i=0; i<ppl;i++){
+$(`#people_name_${i}`).val("");
+$(`#people_exp_${i}`).val("");
+localStorage.removeItem(`people${i+1}_name`);
+localStorage.removeItem(`people${i+1}_exp_storage`);
+localStorage.removeItem(`person${i+1}`);
+}
+ }
+);
 
 // let message = localStorage.message_template +"======" +\n+ localStorage.account_information;
 // console.log('message中身:'+ message)
@@ -286,20 +418,23 @@ $('.create_msg_btn').on('click', function () {
     // console.log('Parseご');
     // console.log(js_pars_obj.name);
 
+    //paymentLineは初期化してあげること
+let paymentLine = '';
+for (let i=0; i< num_ppl; i++){
+paymentLine += `${js_pars_obj[i].name}さん:${js_pars_obj[i].jpy}JPY\n`
+}
 
-let html = `
+    let html = `
 ${localStorage.message_template}
 
 支払い総額:${localStorage.Total_Pay}JPY 
 
 負担額:
-${js_pars_obj[0].name}さん:${js_pars_obj[0].jpy}JPY
-${js_pars_obj[1].name}さん:${js_pars_obj[1].jpy}JPY
-${js_pars_obj[2].name}さん:${js_pars_obj[2].jpy}JPY
+${paymentLine}
 ===========お支払い情報============
 ${localStorage.account_information}
 `
-console.log(html);
+    console.log(html);
 
 
 
